515deb459513e4f6942ed6b3da6288ef4ed9875e
Wei Liu <lwsoftware@gmail.com>
2014-07-16 12:17:16 +0900

    completed

--- Scribbler/CanvasView.m
+++ Scribbler/CanvasView.m
@@ -25,22 +25,29 @@
     return self;
 }
 
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    self.strokes = [[NSMutableArray alloc]init];
+    return self;
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    [super drawRect:rect];
     
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetStrokeColorWithColor(context, [UIColor blackColor].CGColor);
-    
+    CGContextBeginPath(context);
+ 
     // Draw them with a 2.0 stroke width so they are a bit more visible.
-    CGContextSetLineWidth(context, 40.0f);
-    
+    CGContextSetLineWidth(context, 20.0f);
     for (int i=0; i<self.strokes.count; i++) {
         [self drawStroke:(NSArray*)self.strokes[i] withContext:context];
     }
-    
-    // and now draw the Path!
     CGContextStrokePath(context);
 }
 
@@ -49,25 +56,46 @@
     if (points.count>1) {
         CGPoint p = [((NSValue*)points[0]) CGPointValue];
         CGContextMoveToPoint(context, p.x, p.y); //start at this point
-      
+        
         for (int i=1; i<points.count; i++) {
-            CGPoint p = [((NSValue*)points[0]) CGPointValue];
+            CGPoint p = [((NSValue*)points[i]) CGPointValue];
             
             CGContextAddLineToPoint(context, p.x, p.y); //draw to this point
         }
     }
-    
-
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     self.currentStroke = [[NSMutableArray alloc]init];
-//    self.currentStroke addObject:[NSValue valueWithCG]
+    
+    UITouch* touch = [touches anyObject];
+    CGPoint p = [touch locationInView:self];
+    
+    [self.currentStroke addObject:[NSValue valueWithCGPoint:p]];
+    [self.strokes addObject:self.currentStroke];
+     NSLog(@"began: %f, %f", p.x, p.y);
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
+    UITouch* touch = [touches anyObject];
+    CGPoint p = [touch locationInView:self];
+
+    [self.currentStroke addObject:[NSValue valueWithCGPoint:p]];
     
+    [self setNeedsDisplay];
+    
+    NSLog(@"moved: %f, %f", p.x, p.y);
 }
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self setNeedsDisplay];
+  
+    CGPoint p = [[touches anyObject] locationInView:self];
+
+     NSLog(@"end: %f, %f", p.x, p.y);
+}
+
 @end
